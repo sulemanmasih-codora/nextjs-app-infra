d3934a8ba1a9d4806329aa6a1b982c25c48ccbbe
sulemanmasih-codora <131948117+sulemanmasih-codora@users.noreply.github.com>
2023-09-26 17:12:37 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+export NVM_DIR=$HOME/.nvm;
+. $NVM_DIR/nvm.sh;
 
 cd /home/ubuntu/nextjs-app-infra
 git pull origin main
-yarn build &&
-pm2 restart next_hello_world_app
+nvm install v16
+nvm use v16
+npm install --global yarn
+yarn
+yarn build
+npm install pm2 -g
+pm2 delete nextjs-app
+pm2 start npm --name "nextjs-app" -- start
